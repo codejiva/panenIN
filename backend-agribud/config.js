c7e9c81e17f70d8fb8d904e2d0dc20abee5de821
codejiva <rambatilumbungpardja@gmail.com>
2025-05-15 23:37:58 +0700

    fix: minor fix real-time monitoring

--- backend-agribud/config.js
+++ backend-agribud/config.js
@@ -1,6 +1,9 @@
 // jangan diubah-ubah
+require('dotenv').config()
+
 module.exports = {
   port: 5000,
   shapefilePath: './shapefiles/Batas_Kabupaten_BIG_PPBW_V1.shp',
-  geminiApiKey: 'AIzaSyDLlZu3L8gCij2adeXwKWVpL-dcf8EnZgI'
+  geminiApiKey: 'AIzaSyDLlZu3L8gCij2adeXwKWVpL-dcf8EnZgI',
+  simulated: process.env.SIMULATED === 'true'
 };
