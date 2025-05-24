07f324ed39897ac8c8ee4c44e5ffcf657ec9db92
codejiva <rambatilumbungpardja@gmail.com>
2025-05-24 09:25:21 +0700

    small update

--- backend-agribud/config.js
+++ backend-agribud/config.js
@@ -1,9 +1,9 @@
 // jangan diubah-ubah
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
   port: 5000,
   shapefilePath: './shapefiles/Batas_Kabupaten_BIG_PPBW_V1.shp',
-  geminiApiKey: 'AIzaSyDLlZu3L8gCij2adeXwKWVpL-dcf8EnZgI',
+  geminiApiKey: process.env.GEMINI_API_KEY, // Ini yang diubah
   simulated: process.env.SIMULATED === 'true'
 };
