bbbc27e2258fa57b305f5c2ad731e6622ea6a279
codejiva <rambatilumbungpardja@gmail.com>
2025-08-05 09:52:47 +0700

    update certi

--- backend-agribud/config/db.js
+++ backend-agribud/config/db.js
@@ -1,19 +1,28 @@
-// config/db.js
 const mysql = require('mysql2/promise');
+const fs = require('fs');
+const path = require('path');
+
+// TiDB Cloud butuh sertifikat CA khusus buat koneksi SSL
+// buat vercel
+const caPath = path.resolve(__dirname, 'isrgrootx1.pem');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'agribuddy',
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 4000, // gue samain dengan port TiDB Cloud 4000
   ssl: {
-    rejectUnauthorized: true // ! buat di cloud aja ini.
+    // serti CA
+    ca: fs.readFileSync(caPath),
+    // biar aman
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
   },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-console.log('Koneksi pool untuk database MySQL/TiDB dibuat.');
+console.log('Koneksi pool untuk database TiDB Cloud dikonfigurasi.');
 module.exports = pool;
